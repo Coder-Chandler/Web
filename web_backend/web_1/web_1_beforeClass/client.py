fdc239a02888f2560f8fd2172a884498837d8969
Chandler <ysiwgtus@gmail.com>
2017-12-02 15:01:12 +0800

    web_1

---
+++ web_backend/web_1/web_1_beforeClass/client.py
@@ -0,0 +1,65 @@
+# coding: utf-8
+
+import socket
+
+
+# socket 是操作系统用来进行网络通信的底层方案
+# 简而言之, 就是发送 / 接收数据
+
+# 创建一个 socket 对象
+# 参数 socket.AF_INET 表示是 ipv4 协议
+# 参数 socket.SOCK_STREAM 表示是 tcp 协议
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 这两个其实是默认值, 所以你可以不写, 如下
+# s = socket.socket() # http
+# s = ssl.wrap_socket(socket.socket()) # https
+
+# 主机(域名或者ip)和端口
+host = 'baidu.com'
+port = 80
+# 用 connect 函数连接上主机, 参数是一个 tuple
+s.connect((host, port))
+
+# 连接上后, 可以通过这个函数得到本机的 ip 和端口
+ip, port = s.getsockname()
+print('本机 ip 和 port {} {}'.format(ip, port))
+
+# 构造一个 HTTP 请求
+http_request = 'GET / HTTP/1.1\r\nhost:{}\r\n\r\n'.format(host)
+# http_request = 'GET / HTTP/1.1\r\nhost:%s\r\n\r\n' %(host) # 过时的方法！
+
+# 发送 HTTP 请求给服务器
+# send 函数只接受 bytes 作为参数
+# str.encode 把 str 转换为 bytes, 编码是 utf-8
+request = http_request.encode('utf-8')
+print('请求', request)
+s.send(request)
+
+# 接受服务器的响应数据
+# 参数是长度, 这里为 1023 字节
+# 所以这里如果服务器返回的数据中超过 1023 的部分你就得不到了
+response = s.recv(1023)
+
+# 输出响应的数据, bytes 类型
+print('响应', response)
+# 转成 str 再输出
+print('响应的 str 格式', response.decode('utf-8'))
+
+"""
+HTTP/1.1 200 OK
+Date: Sat, 02 Dec 2017 06:27:55 GMT
+Server: Apache
+Last-Modified: Tue, 12 Jan 2010 13:48:00 GMT
+ETag: "51-47cf7e6ee8400"
+Accept-Ranges: bytes
+Content-Length: 81
+Cache-Control: max-age=86400
+Expires: Sun, 03 Dec 2017 06:27:55 GMT
+Connection: Keep-Alive
+Content-Type: text/html
+
+<html>
+<meta http-equiv="refresh" content="0;url=http://www.baidu.com/">
+</html>
+
+"""
