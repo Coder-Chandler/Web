d8b6f955305c8044469771a4e0afb6c95b3d3883
Chandler <ysiwgtus@gmail.com>
2017-12-21 00:34:40 +0800

    Set和Hash表结构的实现

--- web17数据结构与算法分析/Set.py
+++ web17数据结构与算法分析/Set.py
@@ -1,53 +1,74 @@
 class Set(object):
-    # ...
-    def __init__(self, *args):
-        self.data = []
-        for i in args:
-            if i not in self.data:
-                self.data.append(i)
+    def __init__(self):
+        self.table_size = 10007
+        self.table = [0] * self.table_size
 
-    def __repr__(self):
-        return str(self.data)
+    def add(self, key):
+        index = self._index(key)
+        data = [key]
+        v = self.table[index]
+        if isinstance(v, int):
+            self.table[index] = [data]
+        else:
+            self.table.append(data)
 
-    def __str__(self):
-        return str(self.data)
+    def get(self, key):
+        index = self._index(key)
+        v = self.table[index]
+        if isinstance(v, list):
+            for kv in v:
+                if kv[0] == key:
+                    return kv[0]
+        return None
 
-    def __eq__(self, other):
-        pass
+    def remove(self, key):
+        index = self._index(key)
+        v = self.table[index]
+        if isinstance(v, list):
+            for kv in v:
+                if kv[0] == key:
+                    self.table[index].remove(kv)
 
-    def remove(self, x):
-        if x not in self.data:
-            return False
-        for i in range(len(self.data)):
-            if self.data[i] == x:
-                del self.data[i]
+    def __contains__(self, item):
+        return self.has_key(item)
 
-    def add(self, x):
-        if x not in self.data:
-            self.data.append(x)
-
-    def has(self, x):
-        if x in self.data:
-            return True
+    def has_key(self, key):
+        index = self._index(key)
+        v = self.table[index]
+        if isinstance(v, list):
+            for kv in v:
+                if kv[0] == key:
+                    return True
         return False
 
+    def _index(self, key):
+        return self._hash(key) % self.table_size
 
-def testSet():
-    a = Set(1, 2, 2, 3, 4, 4)
-    b = Set(1, 2, 2, 3, 4)
-    c = Set(1, 3, 4, 2)
-    d = Set(2, 3)
-    print('ssssss')
-    print(type(a))
-    # assert (str(a) == '{1, 2, 3, 4}')
-    print(a,b,c,d)
-    # assert (a == b)
-    # assert (a == c)
-    # assert (a != d)
-    # assert (a.has(1) == True)
-    a.remove(1)
-    # assert (a.has(1) == False)
-    a.add(1)
-    # assert (a.has(1) == True)
+    def _hash(self, s):
+        assert isinstance(s, str)
+        n = 1
+        f = 10
+        for i in s:
+            n += ord(i) * f
+            f *= 10
+        return n
 
-testSet()
+
+def test():
+    names = [
+        'xia',
+        'yin',
+        'chandler',
+        'coder',
+    ]
+
+    s = Set()
+    for i in names:
+        s.add(i)
+        print('add 元素', i)
+    s.remove('coder')
+    print('coder 已经被删除，get返回应该是None -> ', s.get('coder'))
+    print('获取 xia 这个key -> ', s.get('xia'))
+
+
+test()
